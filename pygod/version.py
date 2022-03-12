896e9d8f293508aaa4379c7239e63024380fdc3c
Yue Zhao <yzhao062@gmail.com>
2022-03-12 09:56:57 -0500

    setup packaging and unit testing

---
+++ pygod/version.py
@@ -0,0 +1,23 @@
+"""
+``pygod`` is a python library for graph outlier detection
+"""
+# Based on NiLearn package
+# License: simplified BSD
+
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+# X.Y
+# X.Y.Z # For bug fix releases
+#
+# Admissible pre-release markers:
+# X.YaN # Alpha release
+# X.YbN # Beta release
+# X.YrcN # Release Candidate
+# X.Y # Final release
+#
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
+#
+__version__ = '0.1.0'  # pragma: no cover
--- setup.py
+++ setup.py
@@ -1,21 +1,65 @@
 from setuptools import find_packages, setup
 
+# read the contents of README file
+from os import path
+from io import open  # for Python 2 and 3 compatibility
+
+# get __version__ from _version.py
+ver_file = path.join('pyod', 'version.py')
+with open(ver_file) as f:
+    exec(f.read())
+
+this_directory = path.abspath(path.dirname(__file__))
+
+
+# read the contents of README.rst
+def readme():
+    with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+        return f.read()
+
+
+# read the contents of requirements.txt
+with open(path.join(this_directory, 'requirements.txt'),
+          encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+
 setup(name='pygod',
-      version='0.1.0',
+      version=__version__,
       description='Graph Outlier/Anomaly Detection in Python',
+      long_description=readme(),
+      long_description_content_type='text/x-rst',
       author='PyGOD Team',
       author_email='dev@pygod.org',
-      packages=find_packages(),
-      install_requires=[
-          'torch_geometric>=2.0.3'
-      ],
-      package_data={"": ["./*"]},
+      url='https://github.com/pygod-team/pygod/',
+      download_url='https://github.com/pygod-team/pygod/archive/master.zip',
+      keywords=['outlier detection', 'anomaly detection', 'graph mining',
+                'data mining', 'neural networks', 'graph neural networks'],
+      packages=find_packages(exclude=['test']),
       include_package_data=True,
-      license='APACHE',
+      install_requires=requirements,
+      setup_requires=['setuptools>=38.6.0'],
+      license='BSD-2',
+
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Intended Audience :: Education',
+          'Intended Audience :: Financial and Insurance Industry',
+          'Intended Audience :: Science/Research',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Information Technology',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'License :: OSI Approved :: BSD License'
+      ],
+      # not sure about this, decide later
       entry_points={
           'console_scripts': [
               "pygod = pygod.cli.cli:main"
           ]
       },
-      url='https://github.com/pygod-team/pygod',
       )
