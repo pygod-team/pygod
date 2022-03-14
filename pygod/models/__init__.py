29d7fc39ca10879253f3f5972b03e30902c81691
kayzliu <kay.brick.leonard@gmail.com>
2022-03-14 15:11:56 -0500

    change model name

--- pygod/models/__init__.py
+++ pygod/models/__init__.py
@@ -3,6 +3,6 @@ from .mlpae import MLPAE
 from .gcnae import GCNAE
 from .dominant import DOMINANT
 from .one import ONE
-from .guide import Guide
+from .guide import GUIDE
 from .done import DONE
 from .adone import AdONE
