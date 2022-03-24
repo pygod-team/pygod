9e726873b5ddb0d6f3b26af8686ad9ece251ecb1
Yue Zhao <yzhao062@gmail.com>
2022-03-24 11:19:32 -0400

    identify execution issue of ocgnn

---
+++ examples/ocgnn.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""Example of One-Class Graph Neural Networks for Anomaly Detection in
+Attributed Networks
+"""
+# Author: Xueying Ding <xding2@andrew.cmu.edu>
+# License: BSD 2 clause
+
+import torch
+import os.path as osp
+import torch_geometric.transforms as T
+from torch_geometric.datasets import Planetoid
+
+from pygod.models import OCGNN
+from pygod.utils import gen_attribute_outliers, gen_structure_outliers
+from pygod.evaluator.metric import roc_auc_score
+
+
+# data loading
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'Cora')
+data = Planetoid(path, 'Cora', transform=T.NormalizeFeatures())[0]
+
+data, ys = gen_structure_outliers(data, m=10, n=10)
+data, yf = gen_attribute_outliers(data, n=100, k=50)
+data.y = torch.logical_or(torch.tensor(ys), torch.tensor(yf))
+
+# model initialization
+model = OCGNN()
+
+print('training...')
+model.fit(data)
+print()
+
+print('predicting for probability')
+prob = model.predict_proba(data)
+print('Probability', prob)
+print()
+
+print('predicting for raw scores')
+outlier_scores = model.decision_function(data)
+print('Raw scores', outlier_scores)
+print()
+
+print('predicting for labels')
+labels = model.predict(data)
+print('Labels', labels)
+print()
+
+print('predicting for labels with confidence')
+labels, confidence = model.predict(data, return_confidence=True)
+print('Labels', labels)
+print('Confidence', confidence)
+
+print('evaluating outlier detection performance')
+auc_score = roc_auc_score(data.y, outlier_scores)
+print('AUC Score', auc_score)
+print()
--- pygod/models/ocgnn.py
+++ pygod/models/ocgnn.py
@@ -1,56 +1,55 @@
-import time
-import numpy as np
-import torch
-import os
-import torch.nn as nn
 # -*- coding: utf-8 -*-
-"""One-Class Graph Neural Networks for Anomaly Detection in Attributed Networks"""
+""" One-Class Graph Neural Networks for Anomaly Detection in Attributed Networks
+"""
 # Author: Xueying Ding <xding2@andrew.cmu.edu>
 # License: BSD 2 clause
 
-
-
-import logging
-from torch_geometric.nn import GCNConv
-from torch_geometric.data import DataLoader
-from sklearn.metrics import roc_auc_score
-from torch_geometric.utils import to_dense_adj
-from base import BaseDetector
+import numpy as np
+import torch
+import torch.nn as nn
 import torch.nn.functional as F
+
+from torch_geometric.nn import GCNConv
+from sklearn.metrics import roc_auc_score
+from sklearn.utils.validation import check_is_fitted
+
+from . import BaseDetector
+
 
 class GCN_base(nn.Module):
     """
     Describe: Backbone GCN module.
     """
-    def __init__(self,in_feats,n_hidden,n_layers,dropout, act):
+
+    def __init__(self, in_feats, n_hidden, n_layers, dropout, act):
         super(GCN_base, self).__init__()
         self.layers = nn.ModuleList()
         # input layer
         self.layers.append(GCNConv(in_feats, n_hidden, bias=False))
         # hidden layers
         for i in range(n_layers - 1):
-            self.layers.append(GCNConv(n_hidden, n_hidden,  bias=False))
+            self.layers.append(GCNConv(n_hidden, n_hidden, bias=False))
         # output layer
-        self.layers.append(GCNConv(n_hidden, n_hidden,bias=False))
+        self.layers.append(GCNConv(n_hidden, n_hidden, bias=False))
         self.dropout = nn.Dropout(p=dropout)
         self.act = act
 
-        
-    def forward(self,x,edge_index):
+    def forward(self, x, edge_index):
         for i, layer in enumerate(self.layers[0:-1]):
             x = self.dropout(x)
             x = layer(x, edge_index)
             x = self.act(x)
         x = self.dropout(x)
         x = self.layers[-1](x, edge_index)
-        
-        
+
+
 class OCGNN(BaseDetector):
     """
-    OCGNN (One-Class Graph Neural Networks for Anomaly Detection in Attributed Networks):
-    OCGNN is an anomaly detector that measures the distance of anomaly to the centroid, 
-    in the similar fashion to the support vector machine, but in the embedding space after 
-    feeding towards several layers of GCN.
+    OCGNN (One-Class Graph Neural Networks for Anomaly Detection in Attributed
+    Networks): OCGNN is an anomaly detector that measures the distance of
+    anomaly to the centroid, in the similar fashion to the support vector
+    machine, but in the embedding space after feeding towards several layers
+     of GCN.
 
     See: cite 'wang2021one' for details.
 
@@ -89,31 +88,32 @@ class OCGNN(BaseDetector):
     >>> model.fit(data)
     >>> prediction = model.predict(data)
     """
+
     def __init__(self,
-                 n_hidden = 32,
-                 n_layers = 2,
-                 dropout=0, 
+                 n_hidden=32,
+                 n_layers=2,
+                 dropout=0,
                  lr=0.001,
                  weight_decay=5e-4,
-                 eps= 0.001,
-                 nu = 1,
-                 gpu = 0,
-                 epoch = 100,
-                 verbose = True,
-                 act = F.relu):
-        super(OCGNN,self).__init__()
+                 eps=0.001,
+                 nu=1,
+                 gpu=0,
+                 epoch=100,
+                 verbose=True,
+                 act=F.relu):
+        super(OCGNN, self).__init__()
         self.dropout = dropout
         self.n_hidden = n_hidden
         self.n_layers = n_layers
         self.lr = lr
         self.weight_decay = weight_decay
         self.eps = eps
-        self.nu = nu    
+        self.nu = nu
         self.data_center = 0
         self.radius = 0.0
         self.epoch = epoch
-        self.act =act
-        
+        self.act = act
+
         if gpu >= 0 and torch.cuda.is_available():
             self.device = 'cuda:{}'.format(gpu)
         else:
@@ -122,8 +122,7 @@ class OCGNN(BaseDetector):
         # other param
         self.verbose = verbose
         self.model = None
-        
-        
+
     def init_center(self, x, edge_index):
         """
         Descriptions
@@ -143,17 +142,17 @@ class OCGNN(BaseDetector):
         n_samples = 0
         self.model.eval()
         with torch.no_grad():
-            outputs= self.model(x, edge_index)
+            outputs = self.model(x, edge_index)
             # get the inputs of the batch
             n_samples = outputs.shape[0]
             c = torch.sum(outputs, dim=0).to(self.device)
-           # print(outputs)
+        # print(outputs)
         c /= n_samples
         # If c_i is too close to 0, set to +-eps. Reason: a zero unit can be trivially matched with zero weights.
         c[(abs(c) < self.eps) & (c < 0)] = -self.eps
         c[(abs(c) < self.eps) & (c > 0)] = self.eps
         return c
-    
+
     def get_radius(self, dist):
         """
         Description
@@ -169,10 +168,10 @@ class OCGNN(BaseDetector):
         ----------
         r: numpy array: new radius
         """
-        radius=np.quantile(np.sqrt(dist.clone().data.cpu().numpy()), 1 - self.nu)
+        radius = np.quantile(np.sqrt(dist.clone().data.cpu().numpy()),
+                             1 - self.nu)
         return radius
 
-    
     def anomaly_scores(self, outputs):
         """
         Description
@@ -192,7 +191,7 @@ class OCGNN(BaseDetector):
         print(outputs)
         scores = dist - self.radius ** 2
         return dist, scores
-    
+
     def loss_function(self, outputs):
         """
         Description
@@ -209,11 +208,12 @@ class OCGNN(BaseDetector):
         scores: torch Tensor, anomaly scores
         loss: torch Tensor, a combined loss of radius and average scores
         """
-        
-        dist, scores= self.anomaly_scores(outputs)
-        loss = self.radius ** 2 + (1 / self.nu) * torch.mean(torch.max(torch.zeros_like(scores), scores))
+
+        dist, scores = self.anomaly_scores(outputs)
+        loss = self.radius ** 2 + (1 / self.nu) * torch.mean(
+            torch.max(torch.zeros_like(scores), scores))
         return loss, dist, scores
-    
+
     def fit(self, G):
         """
         Description
@@ -232,28 +232,28 @@ class OCGNN(BaseDetector):
         """
         x, edge_index, labels = self.process_graph(G)
         self.in_feats = x.shape[1]
-        
-        print(self.in_feats, self.n_hidden, self.n_layers, self.dropout)
-        
-        #initialize the model and optimizer
-        self.model = GCN_base(self.in_feats,
-                         self.n_hidden, 
-                         self.n_layers,
-                         self.dropout,
-                         self.act)
-        
-        self.optimizer = torch.optim.Adam(self.model.parameters(),
-                                 lr=self.lr,
-                                 weight_decay=self.weight_decay)
 
-        self.data_center= torch.zeros(self.n_hidden, device= self.device)
-        self.radius=torch.tensor(0, device= self.device)
-        
+        print(self.in_feats, self.n_hidden, self.n_layers, self.dropout)
+
+        # initialize the model and optimizer
+        self.model = GCN_base(self.in_feats,
+                              self.n_hidden,
+                              self.n_layers,
+                              self.dropout,
+                              self.act)
+
+        self.optimizer = torch.optim.Adam(self.model.parameters(),
+                                          lr=self.lr,
+                                          weight_decay=self.weight_decay)
+
+        self.data_center = torch.zeros(self.n_hidden, device=self.device)
+        self.radius = torch.tensor(0, device=self.device)
+
         self.model = self.model.to(self.device)
-        #training the model
+        # training the model
         self.model.train()
-        
-        for epoch in range(self.epoch):  
+
+        for epoch in range(self.epoch):
             self.model.zero_grad()
             outputs = self.model(x, edge_index)
             loss, dist, score = self.loss_function(outputs)
@@ -264,18 +264,18 @@ class OCGNN(BaseDetector):
                 loss.backward()
                 self.optimizer.step()
             if self.verbose:
-                print('RRR',self.radius.data)
+                print('RRR', self.radius.data)
             print("Epoch {:05d},loss {:.4f}".format(epoch, loss.item()))
-            
+
             if self.verbose:
                 # TODO: support more metrics
                 print(score.detach().cpu().numpy())
                 print(labels)
                 auc = roc_auc_score(labels, score.detach().cpu().numpy())
-                  
+
                 print("Epoch {:04d}: Loss {:.4f} | AUC {:.4f}"
                       .format(epoch, loss.item(), auc))
-             
+
         self.decision_scores_ = score.detach().cpu().numpy()
         self._process_decision_scores()
         return self
@@ -302,17 +302,16 @@ class OCGNN(BaseDetector):
             Labels of nodes. (currently I use %2 to obtain binary label)
         """
         edge_index = G.edge_index
-        
+
         # TODO: potential memory efficient improvement
         #  via sparse matrix operation
 
         edge_index = edge_index.to(self.device)
         x = G.x.to(self.device)
-        y = G.y %2
+        y = G.y % 2
 
         # return data objects needed for the network
         return x, edge_index, y
-    
 
     def decision_function(self, G):
         """Predict raw anomaly score of X using the fitted detector.
@@ -328,11 +327,10 @@ class OCGNN(BaseDetector):
             The anomaly score of the input samples.
         """
         check_is_fitted(self, ['model'])
-              
-            
+
         # get needed data object from the input data
         x, edge_index, _ = self.process_graph(G)
-        
+
         # enable the evaluation mode
         self.model.eval()
 
@@ -340,14 +338,11 @@ class OCGNN(BaseDetector):
         # outlier_scores = torch.zeros([attrs.shape[0], ])
         outputs = self.model(x, edge_index)
         loss, dist, score = self.loss_function(outputs)
-        
+
         return score.detach().cpu().numpy()
 
-    
     def predict(self, G):
         check_is_fitted(self, ['decision_scores_', 'threshold_', 'labels_'])
         pred_score = self.decision_function(G)
         prediction = (pred_score > self.threshold_).astype('int').ravel()
         return prediction
-
-        
