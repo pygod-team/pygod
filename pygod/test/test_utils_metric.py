3e1132a109899547a13409e9eac323fcdedf554a
yzhao062 <QUV4&g4VTx9V>
2022-04-09 15:14:12 -0400

    improve test coverage

---
+++ pygod/test/test_utils_metric.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+import unittest
+
+# noinspection PyProtectedMember
+from numpy.testing import assert_allclose
+from numpy.testing import assert_equal
+from numpy.testing import assert_raises
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+
+from pygod.utils.metric import eval_roc_auc
+from pygod.utils.metric import eval_recall_at_k
+from pygod.utils.metric import eval_precision_at_k
+
+import numpy as np
+
+
+class TestMetric(unittest.TestCase):
+
+    def setUp(self):
+        self.y = [0, 0, 1, 1, 1, 0, 0, 0, 1, 0]
+        self.decision_scores_ = [0.1, 0.2, 0.2, 0.8, 0.2, 0.5, 0.7, 0.9, 1, 0.3]
+        self.manual_labels = [0, 0, 0, 1, 0, 0, 1, 1, 1, 0]
+
+    def test_eval_roc_auc(self):
+        assert_allclose(roc_auc_score(self.y, self.manual_labels),
+                        eval_roc_auc(self.y, self.decision_scores_))
+
+    def test_eval_recall_at_k(self):
+        assert_allclose(recall_score(self.y, self.manual_labels),
+                        eval_recall_at_k(self.y, self.decision_scores_, k=4))
+
+    def test_eval_precision_at_k(self):
+        assert_allclose(precision_score(self.y, self.manual_labels),
+                        eval_precision_at_k(self.y, self.decision_scores_, k=4))
