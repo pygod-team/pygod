6b27e65544718cc0690ade41a5cde2effcc9fc43
Yingtong Dou <ytongdou@gmail.com>
2023-12-19 16:48:22 -0800

    Add W2 testing

--- pygod/test/test_gadnr.py
+++ pygod/test/test_gadnr.py
@@ -71,7 +71,7 @@ class TestGADNR(unittest.TestCase):
                         lambda_loss3=0.8,
                         real_loss=True,
                         lr=0.01,
-                        epoch=5,
+                        epoch=2,
                         dropout=0.1,
                         weight_decay=0.01,
                         act=torch.nn.functional.relu,
@@ -119,3 +119,45 @@ class TestGADNR(unittest.TestCase):
             detector.predict(self.test_data,
                              return_prob=True,
                              prob_method='something')
+
+    def test_W2(self):
+        detector = GADNR(hid_dim=32,
+                        num_layers=3,
+                        deg_dec_layers=4,
+                        fea_dec_layers=3,
+                        backbone=GCN,
+                        sample_size=2,
+                        sample_time=3,
+                        neigh_loss='W2',
+                        lambda_loss1=0.01,
+                        lambda_loss2=0.1,
+                        lambda_loss3=0.8,
+                        real_loss=True,
+                        lr=0.01,
+                        epoch=2,
+                        dropout=0.1,
+                        weight_decay=0.01,
+                        act=torch.nn.functional.relu,
+                        batch_size=16,
+                        num_neigh=20,
+                        contamination=0.2,
+                        verbose=3,
+                        save_emb=True)
+        detector.fit(self.train_data)
+
+        score = detector.predict(return_pred=False, return_score=True)
+        # assert (eval_roc_auc(self.train_data.y, score) >= self.roc_floor)
+
+        pred, score, conf, emb = detector.predict(self.test_data,
+                                                  return_pred=True,
+                                                  return_score=True,
+                                                  return_conf=True,
+                                                  return_emb=True)
+
+        assert_equal(pred.shape[0], self.test_data.y.shape[0])
+        # assert (eval_roc_auc(self.test_data.y, score) >= self.roc_floor)
+        assert_equal(conf.shape[0], self.test_data.y.shape[0])
+        assert (conf.min() >= 0)
+        assert (conf.max() <= 1)
+        assert_equal(emb.shape[0], self.test_data.y.shape[0])
+        assert_equal(emb.shape[1], detector.hid_dim)
