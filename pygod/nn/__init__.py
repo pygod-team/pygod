241656f2bb3c495f62afb0cfddcb941c0ffdbefb
Yingtong Dou <ytongdou@gmail.com>
2023-12-19 16:24:26 -0800

    add gadnr

--- pygod/nn/__init__.py
+++ pygod/nn/__init__.py
@@ -4,6 +4,7 @@ from .cola import CoLABase
 from .dominant import DOMINANTBase
 from .done import DONEBase
 from .gaan import GAANBase
+from .gadnr import GADNRBase
 from .gae import GAEBase
 from .guide import GUIDEBase
 from .ocgnn import OCGNNBase
@@ -14,5 +15,5 @@ from . import functional
 
 __all__ = [
     "AdONEBase", "AnomalyDAEBase", "CoLABase", "DOMINANTBase", "DONEBase",
-    "GAANBase", "GAEBase", "GUIDEBase", "OCGNNBase"
+    "GAANBase", "GADNRBase", "GAEBase", "GUIDEBase", "OCGNNBase"
 ]
