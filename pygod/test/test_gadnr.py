257b6721852beedbcd8e5d9ac9bb59ecd7465046
Yingtong Dou <ytongdou@gmail.com>
2023-12-19 17:20:13 -0800

    Update test_gadnr.py

--- pygod/test/test_gadnr.py
+++ pygod/test/test_gadnr.py
@@ -56,6 +56,13 @@ class TestGADNR(unittest.TestCase):
             detector.predict(self.train_data,
                              return_prob=True,
                              prob_method='something')
+        
+        with assert_raises(ValueError):
+            pred, score, conf, emb = detector.predict(self.test_data,
+                                                      return_pred=True,
+                                                      return_score=True,
+                                                      return_conf=True,
+                                                      return_emb=True)
 
     def test_sample(self):
         detector = GADNR(hid_dim=32,
@@ -123,24 +130,10 @@ class TestGADNR(unittest.TestCase):
     def test_W2(self):
         detector = GADNR(hid_dim=32,
                         num_layers=3,
-                        deg_dec_layers=4,
-                        fea_dec_layers=3,
-                        backbone=GCN,
-                        sample_size=2,
-                        sample_time=3,
                         neigh_loss='W2',
-                        lambda_loss1=0.01,
-                        lambda_loss2=0.1,
-                        lambda_loss3=0.8,
-                        real_loss=True,
-                        lr=0.01,
                         epoch=2,
-                        dropout=0.1,
-                        weight_decay=0.01,
-                        act=torch.nn.functional.relu,
                         batch_size=16,
                         num_neigh=20,
-                        contamination=0.2,
                         verbose=3,
                         save_emb=True)
         detector.fit(self.train_data)
@@ -161,3 +154,22 @@ class TestGADNR(unittest.TestCase):
         assert (conf.max() <= 1)
         assert_equal(emb.shape[0], self.test_data.y.shape[0])
         assert_equal(emb.shape[1], detector.hid_dim)
+
+    def test_comp_loss(self):
+        detector = GADNR(epoch=5, num_layers=3, real_loss=False)
+        detector.fit(self.train_data)
+
+        pred, score, conf = detector.predict(self.train_data,
+                                             return_pred=True,
+                                             return_score=True,
+                                             return_conf=True)
+
+        assert_equal(pred.shape[0], self.train_data.y.shape[0])
+        assert_equal(conf.shape[0], self.train_data.y.shape[0])
+        assert (conf.min() >= 0)
+        assert (conf.max() <= 1)
+    
+    def test_params(self):
+        with assert_raises(ValueError):
+            detector = GADNR(epoch=5, num_layers=3, neigh_loss='something')
+            detector.fit(self.test_data)
