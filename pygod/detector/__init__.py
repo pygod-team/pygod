bb44560ca473c02b64b8fa73cc22292fde9d1408
Yingtong Dou <ytongdou@gmail.com>
2023-12-19 16:25:06 -0800

    Update __init__.py

--- pygod/detector/__init__.py
+++ pygod/detector/__init__.py
@@ -9,16 +9,16 @@ from .conad import CONAD
 from .dominant import DOMINANT
 from .done import DONE
 from .gaan import GAAN
+from .gadnr import GADNR
 from .gae import GAE
 from .guide import GUIDE
 from .ocgnn import OCGNN
 from .one import ONE
 from .radar import Radar
 from .scan import SCAN
-from .gadnr import GADNR
 
 __all__ = [
     "Detector", "DeepDetector", "AdONE", "ANOMALOUS", "AnomalyDAE", "CoLA",
-    "CONAD", "DOMINANT", "DONE", "GAAN", "GAE", "GUIDE", "OCGNN", "ONE",
-    "Radar", "SCAN", "GADNR"
+    "CONAD", "DOMINANT", "DONE", "GAAN", "GADNR", "GAE", "GUIDE", "OCGNN", "ONE",
+    "Radar", "SCAN"
 ]
